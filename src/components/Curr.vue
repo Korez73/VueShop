6b2183cd1aa042c60ee6cad74b929e37e50c5153
Jamie Weldon <jamie.weldon@outlook.com>
2024-03-04 06:27:57 -0600

    Continue to migrate app from codepen

---
+++ src/components/Curr.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable prettier/prettier -->
+<template>
+    {{ dollar(amt) }}
+</template>
+
+<script>
+export default {
+  props: ['amt'],
+  methods: {
+    dollar(value) {
+      return '$' + Number.parseFloat(value).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style></style>
